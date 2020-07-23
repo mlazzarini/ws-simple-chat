a725b7f539eef61db5b868c0a2a1c2103f463a58
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2020-07-23 19:11:52 +0200

    Disable input and button on message submit

--- client/src/components/NameInput.js
+++ client/src/components/NameInput.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function NameInput(props) {
 	const { enterChatRoom } = props;
 	const [yourName, setYourName] = useState('');
+	const [disabled, setDisabled] = useState(false);
 
 	function handleNameChange(e) {
 		setYourName(e.target.value);
@@ -10,18 +11,20 @@ function NameInput(props) {
 
 	function submitName() {
 		enterChatRoom(yourName);
+		setDisabled(true);
 	}
 
 	return (
 		<div className="name-container">
 			<input
+				disabled={disabled}
 				type="text"
 				placeholder="Name"
 				value={yourName}
 				name="yourName"
 				onChange={handleNameChange}
 			/>
-			<button onClick={submitName}>Join chat room</button>
+			<button onClick={submitName} disabled={disabled}>Join chat room</button>
 		</div>
 	);
 }
